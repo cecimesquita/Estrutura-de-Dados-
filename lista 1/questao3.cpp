5dfc81ae522b4c61ff44270bc1f606efb5a2295b
cecimesquita <cecimcarvalhedom@outlook.com>
2022-09-29 20:23:33 -0300

    01_lista1

---
+++ lista 1/questao3.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+#include<math.h>
+
+void calc esfera(float r, float* area, float* volume){
+    float area1;
+    area1 = 4 * pow(r, 2);
+
+    
+    
+    
+
+
+
+}
